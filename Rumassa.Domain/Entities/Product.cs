aeab17d7bb7574f6036a9bce720f9319280df831
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-05-13 00:01:59 +0500

    added product

---
+++ Rumassa.Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rumassa.Domain.Entities
+{
+    public class Product
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public List<string> PhotoPaths { get; set; }
+        public Guid? UserId { get; set; }
+        public Guid? OrderId { get; set; }
+        public Guid? NewsId { get; set; }
+        public short? CategoryId { get; set; }
+
+    }
+}
