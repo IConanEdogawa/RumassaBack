54a4a09371fb487f3ab4b8b4b1355d0b9dbd280d
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-05-13 00:03:50 +0500

    Added News

---
+++ Rumassa.Domain/Entities/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rumassa.Domain.Entities
+{
+    public class News
+    {
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string CardPhotoPath { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+        public Guid? UserId { get; set; }
+
+    }
+}
